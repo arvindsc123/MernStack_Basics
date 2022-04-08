f3004be71e62fc4e1c8c6d093183327335108c54
Arvind singh chouhan <aschouhan332@gmail.com>
2022-04-08 23:41:55 +0530

    initialize backend and frontend

---
+++ backend/data/notes.js
@@ -0,0 +1,14 @@
+const notes = [
+  {
+    id: "1",
+    title: "dckjsdc",
+    description: "dajdjocndscncjdcnds",
+  },
+  {
+    id: "2",
+    title: "dckadscdscdsjsdc",
+    description: "dajcdscdscsdcdjocndscncjdcnds",
+  },
+];
+
+module.exports = notes;
